f52f67ed622d5e73029a830b931f797225c82bcc
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-03-02 19:24:23 -0800

    Added passportJS oauth serialize desserialize and enabled cookiesession in passport

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,17 +2,22 @@ const passport = require('passport');
 
 module.exports = app => {
 
-    app.get(
+    app.get(//Sign in
         '/auth/google',
         passport.authenticate('google', {
             scope: ['profile', 'email']
         })
     );
 
-    app.get(
+    app.get(// Redirect after sign in
         '/auth/google/callback',
         passport.authenticate('google')
     );
 
+
+    //Get current_user
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
 };
 
