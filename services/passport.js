524770bc93810d7c6c7ea36d557919dbf7956b3c
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-03-02 18:32:08 -0800

    Added ways to handle exsisting google oauth users

--- services/passport.js
+++ services/passport.js
@@ -10,7 +10,16 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id }).save();
+        User.findOne({googleId: profile.id})
+            .then((existingUser) => {
+                if(existingUser) {
+                    //User already exist
+                    done(null, existingUser);
+                }else {
+                    new User({ googleId: profile.id }).save()
+                        .then(user => done(null, user))
+                }
+            })
     })
 );
 
