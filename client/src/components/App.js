d46c382042c2bd0d573ced9ac41367c5133968a4
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-03-11 19:49:03 -0700

    Revert

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,6 @@ const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
-const URL = () => <a  href='https://www.yelp.com/writeareview/biz/yfuIKwjKbQpJ0MKq55q7mA?return_url=%2Fbiz%2FyfuIKwjKbQpJ0MKq55q7mA&source=biz_details_war_button'
-                                      download="Test.pdf">Excellent</a>
 
 const App = () => {
     return (
@@ -14,7 +12,7 @@ const App = () => {
             <BrowserRouter>
                 <div>
                     <Header/>
-                    <Route exact path="/" component={URL}/>
+                    <Route exact path="/" component={Landing}/>
                     <Route exact path={"/surveys"} component={Dashboard}/>
                     <Route path={'/surveys/new'} component={SurveyNew}/>
                 </div>
