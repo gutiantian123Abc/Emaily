b8af7ccef5a7fdf57e3b1ca7b9ddf219cc114931
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-03-17 13:10:28 -0700

    Added Landing component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center'}}>
+            <h1>Nature Cryo</h1>
+            Landing Page
+        </div>
+    );
+
+};
+
+export default Landing;
