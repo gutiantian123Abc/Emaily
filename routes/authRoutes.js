b8e40d42a3ac5897afdc12c106c23224118015bf
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-03-17 11:59:30 -0700

    Added login feature front end

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,7 +10,10 @@ module.exports = app => {
 
     app.get(// Redirect after sign in
         '/auth/google/callback',
-        passport.authenticate('google')
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys');
+        }
     );
 
     app.get('/api/logout', (req, res) => {
