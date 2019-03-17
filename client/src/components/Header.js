fa2ea3d93b946dc4184fcd7aaae1a1ba54463b79
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-03-17 11:26:16 -0700

    Linked Header Content with redux

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component{
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login</a>
+                    </li>
+                );
+
+            default:
+                return (
+                    <li>
+                        <a>Logout</a>
+                    </li>
+                );
+        }
+    }
+
     render() {
         return (
             <div>
@@ -10,7 +31,7 @@ class Header extends Component{
                         Nature Cryo
                       </a>
                       <ul className="right">
-                          <a>Login With Google</a>
+                          {this.renderContent()}
                       </ul>
                   </div>
                </nav>
@@ -19,4 +40,7 @@ class Header extends Component{
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+export default connect(mapStateToProps)(Header);
