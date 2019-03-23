6d4b9afc12d5324e187948638d84ef8e35b99e7f
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-03-23 13:38:35 -0700

    Fixed logout button in Header and added surveynew Link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -22,7 +22,7 @@ class Header extends Component{
                         Credits: {this.props.auth.credits}
                     </li>,
                     <li key="3">
-                        <a href="api/logout">Logout</a>
+                        <a href="/api/logout">Logout</a>
                     </li>
                     ]
                 );
