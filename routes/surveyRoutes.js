5f7202f89891c054dfee24f49f1ff70267da3898
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-03-23 06:52:36 -0700

    Tested out sending email

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,7 +6,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
         const survey = new Survey({
@@ -18,9 +18,17 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now()
         });
-
-        //Place to send Email
-        const mailer = new Mailer(survey, surveyTemplate(survey));
-        mailer.send();
+        
+        try{
+            //Place to send Email
+            const mailer = new Mailer(survey, surveyTemplate(survey));
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
