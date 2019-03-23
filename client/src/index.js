f814204163362bd8bf61e06badef33ee97bd898e
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-03-23 06:00:52 -0700

    Added axios used for api test with cookies

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 
+//axios used for api test with cookies
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
